9d0c20fffb876dea74759537f92ac2aa58a79d20
Nilliam Cris Ometto <nilliamometto@gmail.com>
2024-01-20 18:05:13 -0300

    backend initial commit

---
+++ Backend/src/main/java/com/pgsnapshot/PgsnapshotApplication.java
@@ -0,0 +1,13 @@
+package com.pgsnapshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgsnapshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgsnapshotApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/pgsnapshot/PgsnapshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgsnapshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgsnapshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
